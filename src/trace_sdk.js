d8c190700213c7799f961815df89d17b4a99c0c9
lyx <123>
2019-08-22 20:08:36 +0800

    优化

--- src/trace_sdk.js
+++ src/trace_sdk.js
@@ -13,6 +13,6 @@
   function addScript(url){
     document.write("<script language=javascript src="+url+"></script>");
   }
-  addScript(`${SERVER_HOST}/trace/BuryPointHandler.js`);
+  addScript(`${SERVER_HOST}/trace/BuryPointHandler.js?${Math.random()}`);
 
 }(window));
