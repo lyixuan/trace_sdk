186ed3a64b2a20c59bae55c934f0068eb7f0ea69
zhangshuli <zhangshuli8041@163.com>
2019-11-21 16:08:46 +0800

    env

--- src/trace_sdk.js
+++ src/trace_sdk.js
@@ -1,17 +1,17 @@
-;(function(){
+; (function () {
   'use strict';
 
-  window.env = 'pro'; // dev 走测试环境 pro 走生产环境
+  window.env = 'dev'; // dev 走测试环境 pro 走生产环境
 
   const SERVER_HOST = {
-    dev :'http://172.16.117.64:9999/trace',
-    pro : 'http://bd.ministudy.com/trace'
+    dev: 'http://172.16.117.64:9999/trace',
+    pro: 'http://bd.ministudy.com/trace'
   }[window.env];
 
-  window.examweb = window.env==='dev'?'':'/examweb';
+  window.examweb = window.env === 'dev' ? '' : '/examweb';
 
-  function addScript(url){
-    document.write("<script type='text/javascript'  src="+url+"></script>");
+  function addScript(url) {
+    document.write("<script type='text/javascript'  src=" + url + "></script>");
   }
   addScript(`${SERVER_HOST}/BuryPointHandler.js?v=${Math.random()}`);
   addScript(`${SERVER_HOST}/PathMap.js?v=${Math.random()}`);
